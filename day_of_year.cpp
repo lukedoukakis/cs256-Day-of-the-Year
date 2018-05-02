f11709e8f3cb509e4b8ca398916db696976532dc
lukedoukakis <35055929+lukedoukakis@users.noreply.github.com>
2018-05-01 21:56:05 -0700

    Add files via upload

---
+++ day_of_year.cpp
@@ -0,0 +1,90 @@
+using namespace std;
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+class DayOfYear{
+
+	int rawDay;
+	string month; 	//"January", etc
+	int day; 		//0-31
+
+	public:
+		DayOfYear(int _rawDay){ 				//original constructor
+			rawDay = _rawDay;
+		}
+
+		DayOfYear(string _month, int _day){		//modification constructor
+			if(_day > 31){
+				cout << "Error: amount of days cannot be more than 31";
+			}else{
+				month = _month;
+				day = _day;
+			}
+		}
+
+		void print(){
+
+			int dayArray[12] = {31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+			int monthMarker;
+
+			for(int i = 0; i < 12; i++){
+				if(rawDay <= dayArray[i]){
+					monthMarker= i;
+					break;
+				}
+			}
+
+			switch (monthMarker){
+			case 0:
+				month = "January";
+				day = 31 - dayArray[i]-rawDay;
+				break;
+			case 1:
+				month = "February";
+				day = 28 - dayArray[i]-rawDay;
+				break;
+			case 2:
+				month = "March";
+				day = 31 - dayArray[i]-rawDay;
+				break;
+			case 3:
+				month = "April";
+				day = 30 - dayArray[i]-rawDay;
+				break;
+			case 4:
+				month = "May";
+				day = 31 - dayArray[i]-rawDay;
+				break;
+			case 5:
+				month = "June";
+				day = 30 - dayArray[i]-rawDay;
+				break;
+			case 6:
+				month = "July";
+				day = 31 - dayArray[i]-rawDay;
+				break;
+			case 7:
+				month = "August";
+				day = 31 - dayArray[i]-rawDay;
+				break;
+			case 8:
+				month = "September";
+				day = 30 - dayArray[i]-rawDay;
+				break;
+			case 9:
+				month = "October";
+				day = 31 - dayArray[i]-rawDay;
+				break;
+			case 10:
+				month = "November";
+				day = 30 - dayArray[i]-rawDay;
+				break;
+			case 11:
+				month = "December";
+				day = 31 - dayArray[i]-rawDay;
+				break;
+			}
+			cout << month << " " + day;
+		}
+};
